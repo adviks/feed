523f86349c95377cde726304c2b401db4324ee64
Advik <37414113+adviks@users.noreply.github.com>
2025-03-04 12:04:57 +0000

    sadoadl

--- .eleventy.js
+++ .eleventy.js
@@ -10,30 +10,25 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(readingTime);
-  // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
-  // eleventyConfig.addPlugin(eleventyNavigationPlugin);
-
-  // Configuration API: use eleventyConfig.addLayoutAlias(from, to) to add
-  // layout aliases! Say you have a bunch of existing content using
-  // layout: post. If you don’t want to rewrite all of those values, just map
-  // post to a new file like this:
-  // eleventyConfig.addLayoutAlias("post", "layouts/my_new_post_layout.njk");
-
-  // Merge data instead of overriding
-  // https://www.11ty.dev/docs/data-deep-merge/
+  
   eleventyConfig.setDataDeepMerge(true);
 
   
   // Date formatting (human readable)
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(new Date(dateObj)).toFormat("dd LLL yyyy"); // e.g., "05 Mar 2025"
+  });
+
+  // Time formatting (human readable)
+  eleventyConfig.addFilter("readableTime", (dateObj) => {
+    return DateTime.fromJSDate(new Date(dateObj)).toFormat("hh:mm a"); // e.g., "02:30 PM"
   });
 
   // Date formatting (machine readable)
-  eleventyConfig.addFilter("machineDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+  eleventyConfig.addFilter("machineDate", (dateObj) => {
+    return DateTime.fromJSDate(new Date(dateObj)).toFormat("yyyy-MM-dd"); // e.g., "2025-03-05"
   });
-
+  
   // Minify CSS
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
